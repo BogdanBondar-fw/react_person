48dff92ca7b193c06283bbc9d41fbdb0863b58cd
Bogdan <bondarmfw@gmail.com>
2025-05-13 18:42:14 +0300

    fix1

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,13 +1,13 @@
 export const Person = ({ person }) => (
   <div className="Person">
     <h2 className="Person__name">{person.name}</h2>
-    {person.age && <p className="Person__age">Age: {person.age}</p>}
-    {person.isMarried ? (
-      <p className="Person__partner">
-        My {person.gender === 'm' ? 'wife' : 'husband'} is {person.partnerName}.
-      </p>
-    ) : (
-      <p className="Person__partner">I am not married</p>
+    {person.age !== undefined && (
+      <p className="Person__age">Age: {person.age}</p>
     )}
+    <p className="Person__partner">
+      {person.isMarried
+        ? `${person.partnerName} is my ${person.sex === 'm' ? 'wife' : 'husband'}`
+        : 'I am not married'}
+    </p>
   </div>
 );
